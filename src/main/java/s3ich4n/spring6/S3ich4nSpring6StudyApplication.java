632ea54ad1d152f58ae1631ccc9782c44578dd00
s3ich4n <s3ich4n@gmail.com>
2024-06-26 00:17:51 +0900

    feat: initial commit

---
+++ src/main/java/s3ich4n/spring6/S3ich4nSpring6StudyApplication.java
@@ -0,0 +1,13 @@
+package s3ich4n.spring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ich4nSpring6StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ich4nSpring6StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s3ich4n/spring6/S3ich4nSpring6StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package s3ich4n.spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ich4nSpring6StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
