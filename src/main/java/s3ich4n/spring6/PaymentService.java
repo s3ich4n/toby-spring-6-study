57444aa1c3340f10824c067c1ae8e6a9cf80f1ef
s3ich4n <s3ich4n@gmail.com>
2024-06-26 00:36:40 +0900

    feat: 하는거 보기 (2)
    - 함수 시그니처, 리턴값 테스트를 이렇게 함
    - "일단 돌아간다" 를 보고있음에 집중하자

--- src/main/java/s3ich4n/spring6/PaymentService.java
+++ src/main/java/s3ich4n/spring6/PaymentService.java
@@ -1,13 +1,16 @@
 package s3ich4n.spring6;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.valueOf(0), BigDecimal.ZERO, LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
