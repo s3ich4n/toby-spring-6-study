cc5a44c9647faed85de3c334171bdbe38f7ab97f
s3ich4n <s3ich4n@gmail.com>
2024-06-26 00:59:51 +0900

    feat: 따라치기
    - 의존성 처리
    - 아는대로 코드짜고 빨리 되는거 보기
    - 한 사이클 돌리기

---
+++ src/main/java/s3ich4n/spring6/ExRateData.java
@@ -0,0 +1,11 @@
+package s3ich4n.spring6;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+
+}
--- src/main/java/s3ich4n/spring6/PaymentService.java
+++ src/main/java/s3ich4n/spring6/PaymentService.java
@@ -1,14 +1,45 @@
 package s3ich4n.spring6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.URL;
+import java.net.HttpURLConnection;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.valueOf(0), BigDecimal.ZERO, LocalDateTime.now());
+
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        /**
+         * 환율 가져오기
+         *      E,g., https://open.er-api.com/v6/latest/USD
+         * 금액 계산
+         * 유효시간 계산
+         */
+
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+        
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData exRateData = mapper.readValue(response, ExRateData.class);
+
+        BigDecimal exRate = exRateData.rates().get("KRW");
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
