cdbea49b341492ee605ba207ea7d74170f1bc4d7
s3ich4n <s3ich4n@gmail.com>
2024-06-30 17:33:28 +0900

    refactor: 인터페이스 기반으로 구현
    코드는 변화에는 쉽게 대응할 수 있게 바뀌었다!
    
    그런데, 여전히 Provider를 직접 호출하는 모양새인데...

--- src/main/java/s3ich4n/spring6/Client.java
+++ src/main/java/s3ich4n/spring6/Client.java
@@ -5,10 +5,7 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        // 이 코드만 바꾸면 변화에 보다 더 능동적으로 대처가능
-        // 필요한 구현체를 상속받아서 접근했으니까
-        // PaymentService paymentService = new WebApiExRatePaymentService();
-        PaymentService paymentService = new SimpleExRatePaymentService();
+        PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
