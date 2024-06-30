83686c3027336c7aaf15284b442853d0437b23d7
s3ich4n <s3ich4n@gmail.com>
2024-06-30 16:45:42 +0900

    refactor: 상속을 통한 접근방법
    가져오는 방법, 확장하는 방법을 설명함

--- src/main/java/s3ich4n/spring6/PaymentService.java
+++ src/main/java/s3ich4n/spring6/PaymentService.java
@@ -1,20 +1,13 @@
 package s3ich4n.spring6;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.URL;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class PaymentService {
+abstract public class PaymentService {
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         BigDecimal exRate = getKRWExRate(currency);
@@ -34,29 +27,6 @@ public class PaymentService {
      * @throws JsonProcessingException
      * @throws JsonMappingException
      */
-    private BigDecimal getKRWExRate(String currency)
-            throws MalformedURLException, IOException, JsonProcessingException, JsonMappingException {
-        /**
-         * 환율 가져오기
-         *      E,g., https://open.er-api.com/v6/latest/USD
-         * 금액 계산
-         * 유효시간 계산
-         */
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-        
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData exRateData = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = exRateData.rates().get("KRW");
-        return exRate;
-    }
-
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
+    abstract public BigDecimal getKRWExRate(String currency)
+            throws MalformedURLException, IOException, JsonProcessingException, JsonMappingException;
 }
