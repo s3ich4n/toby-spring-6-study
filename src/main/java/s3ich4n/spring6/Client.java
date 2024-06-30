8cdd11e0d81bd9f45782964f29346eafa4080db8
s3ich4n <s3ich4n@gmail.com>
2024-06-30 22:12:37 +0900

    feat: 캐싱기능 추가
    오브젝트 팩토리만 넣고, 로직은 각 분리된 서비스에 추가하니까 잘됨
    
    의존성 역전까지 함으로서, 인터페이스의 소유권을 payment 쪽으로 넘김.
    policy layer로 옮김으로써 machanism layer가 상위로 의존하도록 할 수 있다

--- src/main/java/s3ich4n/spring6/Client.java
+++ src/main/java/s3ich4n/spring6/Client.java
@@ -2,16 +2,30 @@ package s3ich4n.spring6;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import s3ich4n.spring6.payment.Payment;
+import s3ich4n.spring6.payment.PaymentService;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
+        System.out.println("----------------------------------------------------\n");
+        TimeUnit.SECONDS.sleep(1);
+
+        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment2);
+        System.out.println("----------------------------------------------------\n");
+
+
+        TimeUnit.SECONDS.sleep(3);
+        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment3);
     }
 }
