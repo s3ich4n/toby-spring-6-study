482fde2603f5b603dcdd6b5cc75f04f090c11d8a
s3ich4n <s3ich4n@gmail.com>
2024-06-30 20:51:59 +0900

    feat: Component scan 및 메타 프로그래밍 기법을 이용
    Bean으로 쓸만한 녀석을 코드를 읽고 알아서 파악함

--- src/main/java/s3ich4n/spring6/PaymentService.java
+++ src/main/java/s3ich4n/spring6/PaymentService.java
@@ -1,23 +1,23 @@
 package s3ich4n.spring6;
 
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+/**
+ * 메타 프로그래밍 기법으로 스프링의 BeanFactory 가 얘를 찾을 수 있다
+ */
+@Component
 public class PaymentService {
     private final ExRateProvider exRateProvider;
 
-    // 관심사를 완전히 분리했다.
-    //      더이상 프로바이더 코드는 뭘 쓰는지 알 바 없음.
-    //      이 것이 핵심이다.
     public PaymentService(ExRateProvider exRateProvider) {
         this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 쓰는 사람은 그런거 모르고 걍 쓰면 된다
-        //      근데 이 "쓰는 사람" 은 아직도 "프로바이더" 다.
-        //      결국 이 "그런거 모르고 써도 된다" 를 클라이언트 레벨까지 끌어올리는게 핵심이다
         BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
