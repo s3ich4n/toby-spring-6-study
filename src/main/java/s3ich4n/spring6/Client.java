153d1549168adf899171d3a8e8c754995428b731
s3ich4n <s3ich4n@gmail.com>
2024-06-30 17:48:37 +0900

    refactor: 관계설정 책임의 분리
    관계설정 책임을 분리했다.
    클라이언트는 단순히 프로바이더가 제공하는 옵션대로 쓰면 결과가 나온다.
    
    더이상 프로바이더의 구현체를 까보는 일이 없어도 된다는 뜻이다.
    
    이게 핵심이다.

--- src/main/java/s3ich4n/spring6/Client.java
+++ src/main/java/s3ich4n/spring6/Client.java
@@ -5,7 +5,10 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
+        // 여기에 쓰고싶은 생성자를 넣어주기만 하면 된다
+        // "클라이언트" 코드가 멋대로 갖다 쓰도록 하는게 핵심이다.
+        // "프로바이더"의 코드가 변화에 계속 대응하는게 아니라!
+        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/s3ich4n/spring6/PaymentService.java
+++ src/main/java/s3ich4n/spring6/PaymentService.java
@@ -7,13 +7,17 @@ import java.time.LocalDateTime;
 public class PaymentService {
     private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-        // 어떤 인터페이스를 골라 쓸 수 있는지 선택하게 해준다
-        this.exRateProvider = new WebApiExRateProvider();
+    // 관심사를 완전히 분리했다.
+    //      더이상 프로바이더 코드는 뭘 쓰는지 알 바 없음.
+    //      이 것이 핵심이다.
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         // 쓰는 사람은 그런거 모르고 걍 쓰면 된다
+        //      근데 이 "쓰는 사람" 은 아직도 "프로바이더" 다.
+        //      결국 이 "그런거 모르고 써도 된다" 를 클라이언트 레벨까지 끌어올리는게 핵심이다
         BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
