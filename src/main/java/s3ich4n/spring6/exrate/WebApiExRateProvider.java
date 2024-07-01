4face192530c494c7e5e395731b4e6e9b0309cdc
s3ich4n <s3ich4n@gmail.com>
2024-07-02 00:17:19 +0900

    refactor: 템플릿 분리 (2) 클래스로

--- src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
+++ src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
@@ -1,13 +1,10 @@
 package s3ich4n.spring6.exrate;
 
 import java.math.BigDecimal;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 import s3ich4n.spring6.api.ApiTemplate;
 import s3ich4n.spring6.api.ErApiExRateExtractor;
-import s3ich4n.spring6.api.SimpleApiExecutor;
+import s3ich4n.spring6.api.HttpClientApiExecutor;
 import s3ich4n.spring6.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
@@ -17,18 +14,6 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, uri -> {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(uri)
-                    .GET()
-                    .build();
-
-            try (HttpClient client = HttpClient.newBuilder().build()) {
-                return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-        }, new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
 }
