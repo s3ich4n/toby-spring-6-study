0571a70a1b4ac3dd7b5795768d0e485e4056141f
s3ich4n <s3ich4n@gmail.com>
2024-07-02 00:23:47 +0900

    refactor: 콜백 기본값 지정하기 (1)

--- src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
+++ src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
@@ -3,17 +3,14 @@ package s3ich4n.spring6.exrate;
 import java.math.BigDecimal;
 
 import s3ich4n.spring6.api.ApiTemplate;
-import s3ich4n.spring6.api.ErApiExRateExtractor;
-import s3ich4n.spring6.api.HttpClientApiExecutor;
 import s3ich4n.spring6.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
-    ApiTemplate apiTemplate = new ApiTemplate();    // thread-safe 하게
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     public BigDecimal getExRate(String currency) {
-
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url);
     }
 }
