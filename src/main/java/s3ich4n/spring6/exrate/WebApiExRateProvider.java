1d168b5f103e8d3f9d94e76057af550e075f3c2a
s3ich4n <s3ich4n@gmail.com>
2024-07-02 00:33:08 +0900

    refactor: Spring Container에 빈으로 저장함
    주의: 빈으로 저장할 땐 상태값을 가지지 않는 녀석에만.

--- src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
+++ src/main/java/s3ich4n/spring6/exrate/WebApiExRateProvider.java
@@ -6,7 +6,11 @@ import s3ich4n.spring6.api.ApiTemplate;
 import s3ich4n.spring6.payment.ExRateProvider;
 
 public class WebApiExRateProvider implements ExRateProvider {
-    ApiTemplate apiTemplate = new ApiTemplate();
+    private final ApiTemplate apiTemplate;
+
+    public WebApiExRateProvider(ApiTemplate apiTemplate) {
+        this.apiTemplate = apiTemplate;
+    }
 
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
