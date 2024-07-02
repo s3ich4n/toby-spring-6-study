035879c8b4ce8bf30afc0e6cb136a8ff13c7a32f
s3ich4n <s3ich4n@gmail.com>
2024-07-02 23:26:14 +0900

    feat: application infrastructure bean 등록 (1)
    - 자바에서 데이터를 가져오는 DataSource를 가져오고 Bean으로 만듦
    - 매 트랜잭션마다 EntityManager라는걸 사용하는데, 그걸 해주는 팩토리 빈을 설정
      - JPA를 이용해서, JDBC와 POJO를 SQL로 처리하는 교두보를 만듦
    
    다음 커밋에 들어서면 예외를 어떻게 깔끔하게 처리하는가 볼 수 있음

---
+++ src/main/java/s3ich4n/spring6/DataClient.java
@@ -0,0 +1,28 @@
+package s3ich4n.spring6;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import s3ich4n.spring6.order.Order;
+
+import java.math.BigDecimal;
+
+public class DataClient {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
+        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Order order = new Order("100", BigDecimal.TEN);
+        em.persist(order);
+
+        System.out.println(order);
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+}
---
+++ src/main/java/s3ich4n/spring6/DataConfig.java
@@ -0,0 +1,34 @@
+package s3ich4n.spring6;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());            // 어느 데이터소스를 쓰느냐?
+        emf.setPackagesToScan("s3ich4n.spring6");   // 어느 패키지 밑에 있느냐?
+        // JPA에 원하는 벤더 및 관련 정보를 추가
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
+            setDatabase(Database.H2);
+            setGenerateDdl(true);
+            setShowSql(true);
+        }});
+
+        return emf;
+    }
+}
