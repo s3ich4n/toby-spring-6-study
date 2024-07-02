c990cc3974cfac0aa61c3a564af26b3e5d2b4173
s3ich4n <s3ich4n@gmail.com>
2024-07-03 01:07:02 +0900

    refactor: 예외를 깔끔하게 감싸는 방안 소개
    트랜잭션 매니저, PersistenceContext(자바 표준)을 이용해서 예외를 감쌈
    로그도 Hibernate 에러로 캐치하는게 아니라 스프링 DAO의 Exception으로 처리됨

--- src/main/java/s3ich4n/spring6/DataClient.java
+++ src/main/java/s3ich4n/spring6/DataClient.java
@@ -2,6 +2,9 @@ package s3ich4n.spring6;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import s3ich4n.spring6.data.OrderRepository;
 import s3ich4n.spring6.order.Order;
 
@@ -11,20 +14,22 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute( status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        System.out.println(order);
+                System.out.println(order);
 
-        // 오만가지 예외가 이제 생길 수 있다.
-        //
-        // 버그일 수도 있지만, 진짜 주문이 겹쳐서 에러가 나면? 서비스 정책에 따라 재시도 할 수도 있지 않을까?
-        // 엔진따라 다른 에러가 나면?
-        // JDBC나 MyBatis를 쓰거나 해서 각각 Exception이 나면?
-        //
-        // 결국 변화에 능동적으로 대응할 수 있는 코드를 짜야함 -> 확장성
-        Order order2 = new Order("100", BigDecimal.TEN);
-        repository.save(order2);
+                Order order2 = new Order("100", BigDecimal.TEN);
+                repository.save(order2);
+
+                return null;
+            });
+        } catch (DataIntegrityViolationException e) {
+            System.out.println("주문번호 중복 복구 작업");
+        }
     }
 }
