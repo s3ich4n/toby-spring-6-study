402485bfd963bc82fcba85331f7b9d5b6e248fac
s3ich4n <s3ich4n@gmail.com>
2024-07-02 21:51:12 +0900

    안쓰는 import문 삭제

--- src/test/java/s3ich4n/spring6/TestPaymentConfig.java
+++ src/test/java/s3ich4n/spring6/TestPaymentConfig.java
@@ -2,7 +2,6 @@ package s3ich4n.spring6;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import s3ich4n.spring6.exrate.CachedExRateProvider;
 import s3ich4n.spring6.payment.ExRateProvider;
 import s3ich4n.spring6.payment.ExRateProviderStub;
 import s3ich4n.spring6.payment.PaymentService;
