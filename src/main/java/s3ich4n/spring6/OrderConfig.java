829a2f1e0ad74153000bb298ff9709777db92f49
s3ich4n <s3ich4n@gmail.com>
2024-07-05 00:55:48 +0900

    refactor: JDBC 구현체로 갈아타서 구동해보기
    Configuration은 JPA 내용을 빼고 JDBC내용을 넣어줬다.
    
    JdbcRepository에는 아래 내용을 넣었다.
    
    - Bean 초기화가 끝나고 알아서 돌려주는 `@PostConstruct` annotation이 붙은 initDb() 를 세팅했다
    - 이후 save 로직을 Jdbc 클라이언트에 맞게 처리했다
    - 테스트도 패스했다!
    
    근데 진짜 중요한건
    
    repository 영역만 손을 대고, Bean 설정만 손댔을 뿐. Service 영역은 손도안댔다는 점이다!
    - Order에 손댄건 테스트를 위해 Reflection 쓰는 대신 setter 하나만 쓰려고 한 것임

--- src/main/java/s3ich4n/spring6/OrderConfig.java
+++ src/main/java/s3ich4n/spring6/OrderConfig.java
@@ -3,23 +3,29 @@ package s3ich4n.spring6;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import s3ich4n.spring6.data.JdbcOrderRepository;
-import s3ich4n.spring6.data.JpaOrderRepository;
 import s3ich4n.spring6.order.OrderRepository;
 import s3ich4n.spring6.order.OrderService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
 public class OrderConfig {
-    @Bean
-    public OrderService orderService(JpaTransactionManager jpaTransactionManager) {
-        return new OrderService(orderRepository(), jpaTransactionManager);
-    }
-
     // 구성정보는 이렇게 깔끔하게 치워줄 수 있다
     @Bean
-    public OrderRepository orderRepository() {
-        return new JpaOrderRepository();
+    public OrderRepository orderRepository(DataSource dataSource) {
+        return new JdbcOrderRepository(dataSource);
+    }
+
+    // OrderRepository를 생성자 파라미터로 바로 넘겨주는 방법
+    // 굳이 메소드를 호출하면서 DataSource를 또 직접 넘겨줄 필요가 없어짐
+    @Bean
+    public OrderService orderService(
+            PlatformTransactionManager jpaTransactionManager,
+            OrderRepository orderRepository
+    ) {
+        return new OrderService(orderRepository, jpaTransactionManager);
     }
 }
