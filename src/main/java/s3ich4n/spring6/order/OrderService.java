e05dc0a1e9651650c01cd75ef76225f9952daf5e
s3ich4n <s3ich4n@gmail.com>
2024-07-07 17:52:14 +0900

    feat: 여러 트랜잭션을 하나의 논리적 묶음으로 묶고 그걸 처리하기
    Done
    + 기능은 추가되었고, 기존 트랜잭션 매니저로 논리적으로 끊게도 해놨다
      + 테스트는 당연히 들어간다
        앞으로 별 말 없으면 테스트는 무조건 드간다
    + 이런 코드 사용에 대한 인터페이스가 노출되는게 마음에 안 든다
    
    WIP
    + 스프링은 이걸 해결하는 방법이 있다. 그걸 살펴본다.

--- src/main/java/s3ich4n/spring6/order/OrderService.java
+++ src/main/java/s3ich4n/spring6/order/OrderService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -19,10 +20,14 @@ public class OrderService {
     public Order createOrder(String no, BigDecimal total) {
         Order order = new Order(no, total);
 
-        return new TransactionTemplate(transactionManager).execute(status -> {
-            this.jpaOrderRepository.save(order);
+        this.jpaOrderRepository.save(order);
+        return order;
+    }
 
-            return order;
-        });
+    public List<Order> createOrders(List<OrderReq> reqs) {
+        // 트랜잭션 템플릿을 도입하게 된다
+        return new TransactionTemplate(transactionManager).execute(status ->
+            reqs.stream().map(req -> createOrder(req.no(), req.total())).toList()
+        );
     }
 }
--- src/test/java/s3ich4n/spring6/order/OrderServiceSpringTest.java
+++ src/test/java/s3ich4n/spring6/order/OrderServiceSpringTest.java
@@ -4,17 +4,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import s3ich4n.spring6.OrderConfig;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = OrderConfig.class)
 public class OrderServiceSpringTest {
     @Autowired
     OrderService orderService;
+
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void createOrder() {
@@ -24,5 +31,34 @@ public class OrderServiceSpringTest {
         var order = orderService.createOrder("0100", BigDecimal.TEN);
 
         Assertions.assertThat(order.getId()).isGreaterThan(0L);
+    }
+
+    @Test
+    void createOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.TWO),
+                new OrderReq("0201", BigDecimal.ONE)
+        );
+
+        var orders = orderService.createOrders(orderReqs);
+
+        Assertions.assertThat(orders).hasSize(2);
+        orders.forEach(order -> Assertions.assertThat(order.getId()).isGreaterThan(0L));
+    }
+
+    @Test
+    void createDuplicatedOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.TWO),
+                new OrderReq("0300", BigDecimal.ONE)
+        );
+
+        // 이거는 했는데, 앞의것도 제대로 들어가느냐? 그건 검증이 안됨.
+        Assertions.assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+        JdbcClient client = JdbcClient.create(dataSource);
+        var count = client.sql("select count(*) from orders where no = '0300'").query(Long.class).single();
+        Assertions.assertThat(count).isEqualTo(0L);
     }
 }
